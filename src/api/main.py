c72d84e754df8fda89ff1fac314b9d59a4d0a8c2
Krishna-1906 <adekrishna1906@gmail.com>
2026-06-24 21:38:19 +0530

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,28 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
+products = {
+    "cotton t-shirt": "610910",
+    "leather shoes": "640399",
+    "laptop": "847130",
+    "mobile phone": "851713"
+}
+
 @app.get("/")
 def home():
-  return ("messages": "HS code classifications assistant is running")
+    return {"message": "HS Code Classification Assistant"}
+
+@app.get("/predict/{product}")
+def predict(product: str):
+    hs_code = products.get(product.lower())
+
+    if hs_code:
+        return {
+            "product": product,
+            "hs_code": hs_code
+        }
+
+    return {
+        "error": "Product not found"
+    }
